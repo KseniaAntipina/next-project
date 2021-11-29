c913d58500aa42f14cd317c6c8ee8240c566d48b
Kseniya Antipina <karinpark@mail.ru>
2021-11-29 14:51:20 +0300

    added input component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Htag, Rating, Tag } from "../components";
+import { Htag, Input, Rating, Tag } from "../components";
 import { Button } from "../components";
 import { Ptag } from "../components";
 import { withLayout } from "../layout/Layout";
@@ -19,6 +19,7 @@ function Home({ menu }: HomeProps): JSX.Element {
 			<Ptag size='medium'>Студенты освоят не только hard skills, необходимые для работы веб-дизайнером, но и soft skills — навыки, которые позволят эффективно взаимодействовать в команде с менеджерами, разработчиками и маркетологами. Выпускники факультета могут успешно конкурировать с веб-дизайнерами уровня middle.</Ptag>
 			<Tag size='medium' color='primary'>тег</Tag>
 			<Rating isEditable={true} rating={rating} setRating={setRating} />
+			<Input />
 
 		</>
 	);
